7250b02fb97fe4182fb391141de16415981f02de
Shreyansh <156423676+Shreyygurjar@users.noreply.github.com>
2026-08-26 18:49:12 +0530

    Final Touch added

--- frontend/script.js
+++ frontend/script.js
@@ -2,9 +2,27 @@ const productsContainer = document.querySelector(".products");
 const cartCount = document.getElementById("cart-count");
 const backendTest = document.getElementById("backend-test");
 const backendResult = document.getElementById("backend-result");
+const viewCart = document.getElementById("view-cart");
+const cartItems = document.getElementById("cart-items");
+let cart = [];
+viewCart.addEventListener("click", function() {
 
-let count = 0;
+    cartItems.innerHTML = "";
 
+    if (cart.length === 0) {
+        cartItems.textContent = "Your cart is empty.";
+        return;
+    }
+
+    cart.forEach(function(product) {
+
+        const item = document.createElement("p");
+
+        item.textContent = `${product.name} - ₹${product.price}`;
+
+        cartItems.appendChild(item);
+    });
+});
 
 // Test backend connection
 backendTest.addEventListener("click", function() {
@@ -48,8 +66,8 @@ fetch("http://127.0.0.1:5000/api/products")
             const button = productCard.querySelector("button");
 
             button.addEventListener("click", function() {
-                count++;
-                cartCount.textContent = count;
+                cart.push(product);
+                cartCount.textContent = cart.length;
             });
 
         });
